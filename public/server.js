9c2a75d4a6bfcd1c198bc895c2e1cae8594d2a8b
seokhkang-k <>
2023-02-15 23:36:05 +0900

    2/15 node js

---
+++ public/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'html/detail.html'));
+});
+
+app.listen(3000, () => {
+  console.log('서버가 시작되었습니다.');
+});
