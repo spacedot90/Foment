fa755bb463ae269d2ddf40b2e8d1f95e160c4e5e
seokhkang-k <>
2023-02-15 23:39:41 +0900

    2/15

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
+  res.sendFile(path.join(__dirname, 'public/html/detail.html'));
 });
 
 app.listen(3000, () => {
